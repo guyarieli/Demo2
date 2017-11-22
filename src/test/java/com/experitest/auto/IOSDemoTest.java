131c0f1b1b41b8981ff755dcfd4bb3afdc361a5d
experitest\guy.arieli <guy@experitest.com>
2017-11-22 09:36:38 +0200

    fix url

--- src/test/java/com/experitest/auto/IOSDemoTest.java
+++ src/test/java/com/experitest/auto/IOSDemoTest.java
@@ -24,7 +24,7 @@ public class IOSDemoTest extends BaseTest {
 		//dc.setCapability(MobileCapabilityType.APP, "cloud:com.experitest.ExperiBank");
 		//dc.setCapability(IOSMobileCapabilityType.BUNDLE_ID, "com.experitest.ExperiBank");
 		dc.setCapability("testName", "IOSDemoTest");
-		driver = new IOSDriver<>(new URL(getProperty("url",cloudProperties) + "/wd/hub"), dc);
+		driver = new IOSDriver<>(new URL("https://stage.experitest.com/wd/hub"), dc);
 	}
 
 	@Test
